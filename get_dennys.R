90b8063d229875e15ad0eba1daa3f7b8bf998358
Colin Rundel <rundel@gmail.com>
2016-02-29 16:42:57 -0500

    Multiple zips

--- get_dennys.R
+++ get_dennys.R
@@ -6,24 +6,28 @@ limit = 1000
 radius = 1000
 zipcode = 90210
 
-url = paste0('https://hosted.where2getit.com/dennys/responsive/ajax?&xml_request=',
-             '<request>',
-             '<appkey>6B962D40-03BA-11E5-BC31-9A51842CA48B<%2Fappkey>',
-             '<formdata+id%3D"locatorsearch">',
-               '<dataview>store_default<%2Fdataview>',
-               '<limit>',limit,'<%2Flimit>',
-               '<order>rank%2C_distance<%2Forder>',
-               '<geolocs>',
-                 '<geoloc>',
-                    '<addressline>', zipcode, '<%2Faddressline>',
-                    '<longitude><%2Flongitude>',
-                    '<latitude><%2Flatitude>',
-                    '<country>US<%2Fcountry>',
-                 '<%2Fgeoloc>',
-               '<%2Fgeolocs>',
-               '<stateonly>1<%2Fstateonly>',
-               '<searchradius>', radius, '<%2Fsearchradius>',
-             '<%2Fformdata>',
-             '<%2Frequest>')
+url = function(limit=limit, radius=radius, zip=zip)
+{    
+    paste0('https://hosted.where2getit.com/dennys/responsive/ajax?&xml_request=',
+           '<request>',
+           '<appkey>6B962D40-03BA-11E5-BC31-9A51842CA48B<%2Fappkey>',
+           '<formdata+id%3D"locatorsearch">',
+             '<dataview>store_default<%2Fdataview>',
+             '<limit>',limit,'<%2Flimit>',
+             '<order>rank%2C_distance<%2Forder>',
+             '<geolocs>',
+               '<geoloc>',
+                  '<addressline>', zip, '<%2Faddressline>',
+                  '<longitude><%2Flongitude>',
+                  '<latitude><%2Flatitude>',
+                  '<country>US<%2Fcountry>',
+               '<%2Fgeoloc>',
+             '<%2Fgeolocs>',
+             '<stateonly>1<%2Fstateonly>',
+             '<searchradius>', radius, '<%2Fsearchradius>',
+           '<%2Fformdata>',
+           '<%2Frequest>')
+}
 
-download.file(url, file.path(data_dir, paste0(zipcode,".xml")))
+download.file(url(zip=90210), file.path(data_dir, paste0(90210,".xml")))
+download.file(url(zip=27705), file.path(data_dir, paste0(27705,".xml")))
